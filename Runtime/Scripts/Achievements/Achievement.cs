4c8f321683f370ab1ea68a06981d6e0fa80a54c7
Ibrahim Özhan <89545837+IbrahimOezhan@users.noreply.github.com>
2025-07-30 19:52:54 +0200

    Update

---
+++ Runtime/Scripts/Achievements/Achievement.cs
@@ -0,0 +1,41 @@
+using Sirenix.OdinInspector;
+using System.Text.Json;
+using UnityEngine;
+
+namespace IbrahKit
+{
+    [CreateAssetMenu(fileName = "NewAchievement", menuName ="IbrahKit/Achievement")]
+    public class Achievement : Unlockable
+    {
+        [SerializeField, PreviewField] private Sprite sprite;
+        [SerializeField] private bool secret;
+
+        public (Sprite, string, string) GetData(string secretData, Sprite secretSprite)
+        {
+            Achievement_JsonData data = GetJson(secret? secretData : key);
+
+            Sprite s = secret ? secretSprite : sprite;
+
+            s = IsUnlocked() ? s : Image_Utitlities.GrayscaleSprite(s);
+
+            return (s, data.Title(), data.Description());
+        }
+
+        private Achievement_JsonData GetJson(string json)
+        {
+            JsonSerializerOptions options = new();
+            options.IncludeFields = true;
+
+            try
+            {
+                Achievement_JsonData data = JsonSerializer.Deserialize<Achievement_JsonData>(json, options);
+                return data;
+            }
+            catch
+            {
+                return new();
+            }
+
+        }
+    }
+}
---
+++ Runtime/Scripts/Achievements/Achievement_Manager.cs
@@ -0,0 +1,70 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace IbrahKit
+{
+    public class Achievement_Manager : MonoBehaviour
+    {
+        [SerializeField] private List<Achievement> achievements = new();
+        [SerializeField, Dropdown(Local_Manager.DROP)] private string secretString;
+        [SerializeField] private Sprite secretSprite;
+
+        public Action<string> OnAchievementUnlocked;
+
+        public static Achievement_Manager Instance;
+
+        private void Awake()
+        {
+            if(Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+        }
+
+
+        public void Unlock(string key)
+        {
+            Achievement found = achievements.Find(x => x.GetKey() == key);
+            if(found != null && !found.IsUnlocked())
+            {
+                OnAchievementUnlocked?.Invoke(key);
+                found.Unlock();
+            }
+        }
+
+        public (Sprite, string, string)[] GetAchievements()
+        {
+            (Sprite, string, string)[] result = new (Sprite, string, string)[achievements.Count];
+
+            for (int i = 0; i < achievements.Count; i++)
+            {
+                result[i] = achievements[i].GetData(secretString, secretSprite);
+            }
+
+            return result;
+        }
+    }
+
+    public class Achievement_JsonData
+    {
+        [JsonInclude] private string title;
+        [JsonInclude] private string description;
+
+        public string Title()
+        {
+            return title;
+        }
+
+        public string Description()
+        {
+            return description;
+        }
+    }
+}
