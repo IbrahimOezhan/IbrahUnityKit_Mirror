eb50ce17ebd465eb0137d7c4b25421390f26c909
Ibrahim Özhan <89545837+IbrahimOezhan@users.noreply.github.com>
2025-09-03 15:57:46 +0200

    Update Path_Utilities.cs

--- Runtime/Scripts/Utilities/Path_Utilities.cs
+++ Runtime/Scripts/Utilities/Path_Utilities.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics;
 using System.IO;
+using UnityEditor;
 using UnityEngine;
 
 namespace IbrahKit
@@ -10,7 +11,7 @@ namespace IbrahKit
         private const string myGames = "My Games";
 
 #if UNITY_EDITOR
-        [UnityEditor.MenuItem("Template/OpenPath")]
+        [MenuItem("IbrahKit/OpenPath")]
 #endif
         public static void OpenPath()
         {
