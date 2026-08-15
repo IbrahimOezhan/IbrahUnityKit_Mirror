f5b4a990d8c28f173ee46a1c3ea8ee5dd21e3eff
Ibrahim <ibrahim.oezhan.business@protonmail.com>
2026-08-16 00:36:20 +0200

    Cleaned up Settings UI

--- Runtime/Scripts/Layer6/Settings/UI/UI_Setting_Menu_Item.cs
+++ Runtime/Scripts/Layer6/Settings/UI/UI_Setting_Menu_Item.cs
@@ -13,13 +13,15 @@ namespace IbrahKit.UI
     [Serializable]
     public class UI_Setting_Menu_Item : Menu_Item_Base
     {
-        [SerializeField] private UI_Setting_Map_Element config;
+        [SerializeField] private UI_Setting ui;
+        
+        [SerializeField] public Setting_Config config;
 
         protected override bool Spawn(RectTransform parent, UI_Menu menu, out GameObject go)
         {
-            UI_Setting setting = Object.Instantiate(config.UiSetting, parent);
+            UI_Setting setting = Object.Instantiate(ui, parent);
 
-            Settings_Manager.GetInstance().TryGet(config.GetSettingConfig().GetKey(), out Setting s);
+            Settings_Manager.GetInstance().TryGet(config.GetKey(), out Setting s);
 
             setting.Init(s);
 
